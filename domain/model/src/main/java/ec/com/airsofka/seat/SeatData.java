002cb86f279c468eb6b77ed70fa6ff2758154226
Adrian de los Reyes <adrian.delosreyes@sofka.com.co>
2025-02-07 16:34:19 -0300

    fix: fix number name

--- domain/model/src/main/java/ec/com/airsofka/seat/SeatData.java
+++ domain/model/src/main/java/ec/com/airsofka/seat/SeatData.java
@@ -11,22 +11,22 @@ public class SeatData {
 
         // FIRST CLASS (2 columns per row, 2 rows)
         for (int i = 0; i < 2; i++, rowNumber++) {
-            seats.add(new SeatCreatedDTO("F" + rowNumber + "A", rowNumber, "A", SeatClass.FIRST, SeatStatus.AVAILABLE, new BigDecimal("500.00"), idFlight));
-            seats.add(new SeatCreatedDTO("F" + rowNumber + "B", rowNumber, "B", SeatClass.FIRST, SeatStatus.AVAILABLE, new BigDecimal("500.00"), idFlight));
+            seats.add(new SeatCreatedDTO(rowNumber + "A", rowNumber, "A", SeatClass.FIRST, SeatStatus.AVAILABLE, new BigDecimal("500.00"), idFlight));
+            seats.add(new SeatCreatedDTO(rowNumber + "B", rowNumber, "B", SeatClass.FIRST, SeatStatus.AVAILABLE, new BigDecimal("500.00"), idFlight));
         }
 
         // BUSINESS CLASS (4 columns per row, 3 rows)
         for (int i = 0; i < 3; i++, rowNumber++) {
-            seats.add(new SeatCreatedDTO("B" + rowNumber + "A", rowNumber, "A", SeatClass.BUSINESS, SeatStatus.AVAILABLE, new BigDecimal("300.00"), idFlight));
-            seats.add(new SeatCreatedDTO("B" + rowNumber + "B", rowNumber, "B", SeatClass.BUSINESS, SeatStatus.AVAILABLE, new BigDecimal("300.00"), idFlight));
-            seats.add(new SeatCreatedDTO("B" + rowNumber + "C", rowNumber, "C", SeatClass.BUSINESS, SeatStatus.AVAILABLE, new BigDecimal("300.00"), idFlight));
-            seats.add(new SeatCreatedDTO("B" + rowNumber + "D", rowNumber, "D", SeatClass.BUSINESS, SeatStatus.AVAILABLE, new BigDecimal("300.00"), idFlight));
+            seats.add(new SeatCreatedDTO(rowNumber + "A", rowNumber, "A", SeatClass.BUSINESS, SeatStatus.AVAILABLE, new BigDecimal("300.00"), idFlight));
+            seats.add(new SeatCreatedDTO(rowNumber + "B", rowNumber, "B", SeatClass.BUSINESS, SeatStatus.AVAILABLE, new BigDecimal("300.00"), idFlight));
+            seats.add(new SeatCreatedDTO(rowNumber + "C", rowNumber, "C", SeatClass.BUSINESS, SeatStatus.AVAILABLE, new BigDecimal("300.00"), idFlight));
+            seats.add(new SeatCreatedDTO(rowNumber + "D", rowNumber, "D", SeatClass.BUSINESS, SeatStatus.AVAILABLE, new BigDecimal("300.00"), idFlight));
         }
 
         // ECONOMY CLASS (6 columns per row, 5 rows)
         for (int i = 0; i < 5; i++, rowNumber++) {
             for (char col = 'A'; col <= 'F'; col++) {
-                seats.add(new SeatCreatedDTO("E" + rowNumber + col, rowNumber, String.valueOf(col), SeatClass.ECONOMY, SeatStatus.AVAILABLE, new BigDecimal("100.00"), idFlight));
+                seats.add(new SeatCreatedDTO( rowNumber +  String.valueOf(col), rowNumber, String.valueOf(col), SeatClass.ECONOMY, SeatStatus.AVAILABLE, new BigDecimal("100.00"), idFlight));
             }
         }
 
