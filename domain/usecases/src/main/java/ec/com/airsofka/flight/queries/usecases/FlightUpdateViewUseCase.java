f5416b5270f5776603ba96ca2e9d06137c2f9258
Adrian de los Reyes <adrian.delosreyes@sofka.com.co>
2025-02-09 23:53:35 -0300

    Revert "Revert "UpdateFlight""

--- domain/usecases/src/main/java/ec/com/airsofka/flight/queries/usecases/FlightUpdateViewUseCase.java
+++ domain/usecases/src/main/java/ec/com/airsofka/flight/queries/usecases/FlightUpdateViewUseCase.java
@@ -4,16 +4,15 @@ import ec.com.airsofka.gateway.IFlightRepository;
 import ec.com.airsofka.gateway.dto.FlightDTO;
 import ec.com.airsofka.generics.interfaces.IUseCaseAccept;
 
-public class FlightSavedViewUseCase implements IUseCaseAccept<FlightDTO, Void> {
-
+public class FlightUpdateViewUseCase implements IUseCaseAccept<FlightDTO, Void>  {
     private final IFlightRepository flightRepository;
 
-    public FlightSavedViewUseCase(IFlightRepository flightRepository) {
+    public FlightUpdateViewUseCase(IFlightRepository flightRepository) {
         this.flightRepository = flightRepository;
     }
 
     @Override
-    public void accept(FlightDTO flightDTO) {
-        flightRepository.save(flightDTO).subscribe();
+    public void accept(FlightDTO request) {
+        flightRepository.save(request).subscribe();
     }
 }
